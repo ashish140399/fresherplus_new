5749004248ed185b62a797cc882ab71bb40e4000
ashish140399 <ashish140399@gmail.com>
2019-12-29 22:57:32 +0530

    change in index

--- js/script.js
+++ js/script.js
@@ -1,44 +1,3 @@
-//Pointing to contact START
-$('.hb_toggle').css("display","none");
-	var jump=function(e)
-{
-   if (e){
-       e.preventDefault();
-
-       var target = $(this).attr("href");
-   }else{
-       var target = location.hash;
-   }
-
-   $('html,body').animate(
-   {
-       scrollTop: $(target).offset().top
-   },9000,function()
-   {
-       location.hash = target;
-       
-   });
-
-}
-
-$('html, body').hide();
-
-$(document).ready(function()
-{
-    $('a[href^="#"]').on("click", jump);
-
-
-    if (location.hash){
-        setTimeout(function(){
-            $('html, body').scrollTop(0).show();
-            jump();
-        }, 5000);
-    }else{
-        $('html, body').show();
-    }
-});
-//Pointing to contact END
-
 
 $(document).ready(function() {
     $("[href]").each(function() {
@@ -47,3 +6,16 @@ $(document).ready(function() {
         }
     });
 });
+
+
+
+
+$(document).ready(function() {
+        
+        $('.Returnship_btnfaq').click(function (e) {
+          e.preventDefault();
+          scrollToElement('FAQ.html#FAQ_Returnship', 5000);
+        });
+
+      
+    });
